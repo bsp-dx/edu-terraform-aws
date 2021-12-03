15543884d9f04e66abb905a7b4130039beb09541
seonbo.shim <seonbo.shim@bespinglobal.com>
2021-12-03 23:25:33 +0900

    ECS Task Definition 생성을 위한 ECS Task Execution Role 구성 추가

---
+++ ecs-tasks-role.tf
@@ -0,0 +1,34 @@
+locals {
+  ecs_task_role_name = "${var.context.project}EcsTaskExecutionRole"
+}
+
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  count = var.create_ecs ? 1 : 0
+
+  name               = "latency-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  tags = merge(var.context.tags, { Name = local.ecs_task_role_name })
+}
+
+data "aws_iam_policy" "ecs_task_execution_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  count = var.create_ecs ? 1 : 0
+
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = data.aws_iam_policy.ecs_task_execution_policy.arn
+}
--- output.tf
+++ output.tf
@@ -12,3 +12,8 @@ output "ecs_cluster_name" {
   description = "The name of the ECS cluster"
   value       = local.cluster_name
 }
+
+output "ecs_task_execution_role_arn" {
+  description = "The ARN of ECS task execution role"
+  value       = concat(aws_iam_role.ecs_task_execution_role.*.arn, [""])[0]
+}
