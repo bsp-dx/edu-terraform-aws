18b633b2c2b414dd072ddde4d6c816b5301f6f35
seonbo.shim <seonbo.shim@bespinglobal.com>
2021-12-08 00:26:48 +0900

    tfmodule-aws-ecs-v1.0.0
    EC2 instance role 및 instance profile role 자동 생성 추가

--- iam-ec2-role.tf
+++ iam-ec2-role.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 resource "aws_iam_role" "this" {
-  count = var.create_ecs && var.create_ecs_instance_role ? 1 : 0
+  count = var.create_ecs && var.create_ec2_instance_role ? 1 : 0
 
   name = "${local.iam_name_prefix}EcsInstanceRole"
   path = "/ecs/"
@@ -31,7 +31,7 @@ EOF
 }
 
 resource "aws_iam_instance_profile" "this" {
-  count = var.create_ecs && var.create_ecs_instance_role ? 1 : 0
+  count = var.create_ecs && var.create_ec2_instance_role ? 1 : 0
 
   name = "${local.iam_name_prefix}EcsInstanceProfile"
   role = concat(aws_iam_role.this.*.name, [""])[0]
@@ -40,21 +40,21 @@ resource "aws_iam_instance_profile" "this" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_ec2_role" {
-  count = var.create_ecs && var.create_ecs_instance_role ? 1 : 0
+  count = var.create_ecs && var.create_ec2_instance_role ? 1 : 0
 
   role       = concat(aws_iam_role.this.*.id, [""])[0]
   policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 resource "aws_iam_role_policy_attachment" "amazon_ssm_managed_instance_core" {
-  count = var.create_ecs && var.create_ecs_instance_role ? 1 : 0
+  count = var.create_ecs && var.create_ec2_instance_role ? 1 : 0
 
   role       = concat(aws_iam_role.this.*.id, [""])[0]
   policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_ec2_cloudwatch_role" {
-  count = var.create_ecs && var.create_ecs_instance_role ? 1 : 0
+  count = var.create_ecs && var.create_ec2_instance_role ? 1 : 0
 
   role       = concat(aws_iam_role.this.*.id, [""])[0]
   policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/CloudWatchLogsFullAccess"
