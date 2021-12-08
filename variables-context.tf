d0dd1ea09a4f0e34ab43fea41d32c82622c185f1
seonbo.shim <seonbo.shim@bespinglobal.com>
2021-12-08 14:38:48 +0900

    cleansing

--- variables-context.tf
+++
@@ -1,8 +0,0 @@
-variable "context" {
-  description = "Provides standardized naming policy and attribute information for data source reference to define cloud resources for a Project."
-  type        = object({
-    project     = string # project name
-    name_prefix = string # resource name prefix
-    tags        = map(string)
-  })
-}
